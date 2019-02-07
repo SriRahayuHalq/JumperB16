b35e57aa4cdf70493c9518b06d5e2ff858eaf977
SriRahayuHalq <47411258+SriRahayuHalq@users.noreply.github.com>
2019-02-07 17:11:53 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1549013125,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground_stone-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/gold_-sheet0.png",
+		"images/gold_-sheet1.png",
+		"images/gold_-sheet2.png",
+		"images/gameover-sheet0.png",
+		"images/background2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
